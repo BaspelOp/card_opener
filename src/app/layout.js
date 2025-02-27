2006702cdb8745d0103f0eb02ad8232346b6e505
Baspel <brianovkolp@gmail.com>
2025-02-27 13:05:15 +0100

    refactor: change layout and made layoutWrapper

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import LayoutWrapper from "./layoutWrapper";
 
 export const metadata = {
   title: "Card Opener",
@@ -17,13 +6,5 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en" className="h-full bg-white">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full bg-white text-black`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+  return <LayoutWrapper>{children}</LayoutWrapper>;
 }
---
+++ src/app/layoutWrapper.js
@@ -0,0 +1,43 @@
+"use client";
+
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import NavBar from "./components/navbar";
+import { usePathname } from "next/navigation";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function LayoutWrapper({ children }) {
+  const pathname = usePathname();
+
+  if (pathname === "/login" || pathname === "/register") {
+    return (
+      <html lang="en" className="h-full bg-white select-none">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased h-full bg-white text-black`}
+        >
+          {children}
+        </body>
+      </html>
+    );
+  }
+
+  return (
+    <html lang="en" className="h-full bg-white select-none">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full bg-white text-black`}
+      >
+        <NavBar />
+        {children}
+      </body>
+    </html>
+  );
+}
