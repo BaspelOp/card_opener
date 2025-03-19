2023881dd21634bb544f18dab1a3247118fd88ae
Baspel <brianovkolp@gmail.com>
2025-03-19 12:46:29 +0100

    refactor: change dummy datat to idk

--- postgres/db-init/insert-data.sql
+++ postgres/db-init/insert-data.sql
@@ -1,32 +1,32 @@
-CALL register('example1', '{"local": "example1","domain": "example.com"}', 'password1');
-CALL register('example2', '{"local": "example2","domain": "example.com"}', 'password2');
+CALL register('john_doe', '{"local": "john.doe","domain": "database.cz"}', 'securePass123');
+CALL register('jane_smith', '{"local": "jane.smith","domain": "company.com"}', 'strongPassword456');
 
 INSERT INTO packages (name, number_of_cards, image_path) VALUES 
-('package1', 7, 'path/to/package1.png');
+('Package Of Water', 7, 'images/packages/package_water.png');
 
 INSERT INTO collections (name) VALUES 
-('collection1');
+('Elements');
 
 -- Numbers from document but optimized
 INSERT INTO card_rarities (name, chance, image_path) VALUES
-('Common', 999, 'path/to/common.png'),
-('Uncommon', 199, 'path/to/uncommon.png'),
-('Rare', 40, 'path/to/rare.png'),
-('Mythical', 8, 'path/to/mythical.png'),
-('Legendary', 4, 'path/to/legendary.png');
+('Common', 999, 'images/rarity/common.png'),
+('Uncommon', 199, 'images/rarity/uncommon.png'),
+('Rare', 40, 'images/rarity/rare.png'),
+('Mythical', 8, 'images/rarity/mythical.png'),
+('Legendary', 4, 'images/rarity/legendary.png');
 
 INSERT INTO card_frames (name, image_path) VALUES 
-('frame1', 'path/to/frame1.png');
+('Water Frame', 'images/frames/water_frame.png');
 
 INSERT INTO card_icons (name, image_path) VALUES 
-('icon1', 'path/to/icon1.png');
+('Water Icon', 'images/icons/water_icon.png');
 
 INSERT INTO cards (name, image_path, collection_id, rarity_id, frame_id, icon_id) VALUES 
-('card1', 'path/to/card1.png', 1, 1, 1, 1),
-('card2', 'path/to/card2.png', 1, 2, 1, 1),
-('card3', 'path/to/card3.png', 1, 3, 1, 1),
-('card4', 'path/to/card4.png', 1, 4, 1, 1),
-('card5', 'path/to/card5.png', 1, 5, 1, 1);
+('Human Of Water', 'images/cards/human_of_water.png', 1, 1, 1, 1),
+('Guard Of Water', 'images/cards/guard_of_water.png', 1, 2, 1, 1),
+('Prisoner Of Water', 'images/cards/prisoner_of_water.png', 1, 3, 1, 1),
+('Water Element', 'images/cards/water_element.png', 1, 4, 1, 1),
+('Shark', 'images/cards/shark.png', 1, 5, 1, 1);
 
 INSERT INTO owned_cards (user_id, card_id, quantity) VALUES 
 (1, 1, 10), 
