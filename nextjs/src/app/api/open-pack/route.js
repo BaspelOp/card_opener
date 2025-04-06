bff039d1b90d167fc15b76cbe43b9dd9f46f99f4
Baspel <brianovkolp@gmail.com>
2025-04-06 20:51:53 +0200

    feat: add get packs, open packs

--- nextjs/src/app/api/open-pack/route.js
+++ nextjs/src/app/api/open-pack/route.js
@@ -1,39 +1,66 @@
 import { NextResponse } from "next/server";
-
-const cards = [
-  { id: 1, name: "Fire Dragon", rarity: "common", chance: 0.5, holo: true },
-  { id: 2, name: "Ice Phoenix", rarity: "rare", chance: 0.3, holo: false },
-  { id: 3, name: "Thunder Wolf", rarity: "common", chance: 0.5, holo: false },
-  {
-    id: 4,
-    name: "Golden Griffin",
-    rarity: "legendary",
-    chance: 0.1,
-    holo: true
-  },
-  { id: 5, name: "Shadow Serpent", rarity: "rare", chance: 0.3, holo: false }
-];
-
-function getRandomCard(count) {
-  return Array.from({ length: count }, () => {
-    let random = Math.random();
-    let cumulative = 0;
-    return cards.find((card) => (cumulative += card.chance) > random);
-  });
-}
+import pool from "@/lib/db";
 
 export async function POST(request) {
   const { userId, packId } = await request.json();
 
-  if (!userId || !packId) {
-    return NextResponse.json({ error: "Missing parameters" }, { status: 400 });
+  console.log("User ID:", userId, "Pack ID:", packId);
+
+  try {
+    if (!userId || !packId) {
+      return NextResponse.json({ error: "Missing parameters" }, { status: 400 });
+    }
+
+    const client = await pool.connect();
+
+    const result = await client.query(
+      'SELECT get_random_cards_from_package($1) AS result',
+      [packId]
+    );
+
+    const cardIds = result.rows[0]?.result;
+
+    console.log("Random card IDs:", cardIds);
+
+    if (!cardIds || cardIds.length === 0) {
+      return NextResponse.json({ error: "No cards found" }, { status: 404 });
+    }
+
+    const cardDetailsQuery = `
+      SELECT 
+        cards.id AS card_id,
+        cards.name AS card_name,
+        cards.image_path AS card_image,
+        collections.name AS collection_name,
+        card_rarities.name AS rarity_name,
+        card_rarities.chance AS rarity_chance,
+        card_frames.name AS frame_name,
+        card_frames.image_path AS frame_image,
+        card_icons.name AS icon_name,
+        card_icons.image_path AS icon_image
+      FROM cards
+      JOIN collections ON cards.collection_id = collections.id
+      JOIN card_rarities ON cards.rarity_id = card_rarities.id
+      JOIN card_frames ON cards.frame_id = card_frames.id
+      JOIN card_icons ON cards.icon_id = card_icons.id
+      WHERE cards.id = ANY ($1);
+    `;
+    const cardDetailsResult = await client.query(cardDetailsQuery, [cardIds]);
+
+    const detailedCardsMap = new Map();
+    for (const row of cardDetailsResult.rows) {
+      detailedCardsMap.set(row.card_id, row);
+    }
+
+    const detailedCards = cardIds.map((id) => detailedCardsMap.get(id));
+
+    client.release();
+
+    console.log("Detailed Cards:", detailedCards);
+
+    return NextResponse.json({ cards: detailedCards }, { status: 200 });
+  } catch (err) {
+    console.error("Error:", err);
+    return NextResponse.json({ error: err.message }, { status: 500 });
   }
-
-  const openedCards = getRandomCard(3);
-
-  // Save openedCards to the user database
-
-  return NextResponse.json({
-    cards: openedCards
-  });
 }
--- nextjs/src/app/pack/page.jsx
+++ nextjs/src/app/pack/page.jsx
@@ -1,17 +1,27 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
 import { motion } from "framer-motion";
 import Tilt from "react-parallax-tilt";
 
 export default function Pack({ userId }) {
   const [opened, setOpened] = useState(false);
   const [cards, setCards] = useState([]);
+  const [packs, setPacks] = useState([]);
+  const { data: session, status } = useSession();
 
-  const openPack = async () => {
+  let hasFetched = false;
+
+  const openPack = async (packId) => {
+    if (!session) {
+      alert("Musíte být přihlášeni, abyste mohli otevřít balíček.");
+      return;
+    }
+
     const response = await fetch("/api/open-pack", {
       method: "POST",
-      body: JSON.stringify({ userId: 1, packId: 1 }),
+      body: JSON.stringify({ userId: session.user.id, packId }),
       headers: {
         "Content-Type": "application/json"
       }
@@ -24,17 +34,52 @@ export default function Pack({ userId }) {
     }
   };
 
+  const fetchPacks = async () => {
+    if (hasFetched) return;
+    hasFetched = true;
+
+    const response = await fetch("api/get-packs", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      setPacks(data);
+    }
+  }
+
+  useEffect(() => {
+    fetchPacks();
+  }, []);
+
   return (
     <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
       {!opened ? (
-        <motion.div
-          className="w-48 h-64 bg-yellow-500 rounded-lg flex items-center justify-center text-white font-bold text-xl shadow-lg cursor-pointer pointer-events-auto"
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          onClick={openPack}
-        >
-          Otevřít balíček
-        </motion.div>
+        <>
+          {packs.map((pack, _) => (
+          <motion.div
+            key={pack.id}
+            className="w-48 h-64 bg-blue-600 rounded-lg flex items-center justify-center text-white font-bold text-xl shadow-lg cursor-pointer pointer-events-auto"
+            // style={{
+            //   backgroundImage: `url(${image_path})`,
+            //   backgroundSize: "cover",
+            //   backgroundPosition: "center"
+            // }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            onClick={() => openPack(pack.id)}
+          >
+            <div className="text-center">
+              {pack.name}
+              <div className="text-sm text-gray-200">Počet karet: {pack.number_of_cards}</div>
+            </div>
+          </motion.div>
+        ))}
+        </>
       ) : (
         <>
           <div className="flex flex-col items-center pointer-events-auto">
@@ -45,7 +90,7 @@ export default function Pack({ userId }) {
                   tiltMaxAngleY={20}
                   perspective={1000}
                   scale={1.05}
-                  glareEnable={card.holo}
+                  glareEnable={card.rarity_name === "Mythical" || card.rarity_name === "Legendary" || card.holo}
                   glareMaxOpacity={0.45}
                   glareColor="white"
                   glarePosition="all"
@@ -53,27 +98,28 @@ export default function Pack({ userId }) {
                 >
                   <motion.div
                     key={card.id}
-                    className={`w-32 h-48 p-4 flex items-center justify-center text-white font-bold text-lg rounded-lg shadow-lg ${card.rarity === "legendary" ? "bg-purple-500" : card.rarity === "rare" ? "bg-blue-500" : "bg-gray-500"}`}
+                    className={`w-32 h-48 p-4 flex items-center justify-center text-white font-bold text-lg rounded-lg shadow-lg ${card.rarity_name === "Legendary" ? "bg-amber-400" : card.rarity_name === "Rare" ? "bg-blue-500" : "bg-gray-500"}`}
                     initial={{ opacity: 0, y: -50 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ delay: index * 0.3 }}
                   >
-                    {card.name}
+                    <div className="text-center">
+                      {card.card_name}
+                      <div className="text-sm text-gray-200">Kolekce: {card.collection_name}</div>
+                    </div>
                   </motion.div>
                 </Tilt>
               ))}
             </div>
-            <div className="w-full text-center mt-4">
-              <div
-                className="text-blue-500 cursor-pointer block"
+            <button
+                className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 hover:cursor-pointer"
                 onClick={() => {
                   setOpened(false);
                   setCards([]);
                 }}
               >
-                Otevřít další balíček
-              </div>
-            </div>
+                Zpět na balíčky
+              </button>
           </div>
         </>
       )}
