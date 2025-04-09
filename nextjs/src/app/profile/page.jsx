61715d082b80e8e894e8b335d6d98ab2d5b0e176
Baspel <brianovkolp@gmail.com>
2025-04-09 14:31:36 +0200

    refactor: profile

--- nextjs/src/app/profile/page.jsx
+++ nextjs/src/app/profile/page.jsx
@@ -1,12 +1,36 @@
 'use client';
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useEffect } from "react";
 
 export default function ProfileComponent() {
   const { data: session, status } = useSession();
+  let hasFetched = false;
 
   console.log("Session data:", session);
   console.log("Session status:", status);
 
+  const loadUserData = async () => {
+    if (hasFetched) return;
+    if (!session) return;
+
+    hasFetched = true;
+
+    const response = await fetch("/api/user-data", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+
+    if (!response.ok) return;
+
+    const data = await response.json();
+    console.log("User data:", data);
+  }
+
+  useEffect(() => {
+    loadUserData();
+  }, []);
 
   if (status === "loading") {
     return (
@@ -31,26 +55,68 @@ export default function ProfileComponent() {
   }
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100 py-10">
-      <div className="flex justify-center items-center flex-col max-w-4xl mx-auto bg-white rounded-lg shadow-md p-6 w-full">
-        <h1 className="text-3xl font-bold mb-4">Profil</h1>
-        <div className="flex flex-col gap-4">
-          <div>
-            <p className="text-gray-600 text-sm">Jméno:</p>
-            <p className="text-lg font-medium">{session.user.username || "Neznámé jméno"}</p>
-          </div>
-          <div>
-            <p className="text-gray-600 text-sm">Email:</p>
-            <p className="text-lg font-medium">{session.user.email}</p>
-          </div>
-          {session.user.role && (
-            <div>
-              <p className="text-gray-600 text-sm">Role:</p>
-              <p className="text-lg font-medium">{session.user.role}</p>
+    <>
+      <div className="flex justify-content items-center h-screen bg-gray-100 py-16">
+        <div className="flex justify-center bg-white shadow-md rounded-lg p-6 w-1/4 h-full ml-10 mt-15">
+          <div className="flex flex-col items-center gap-4">
+            <img src="/avatar.png" alt="Avatar picture" className="w-48" />
+
+            <div className="w-full text-left">
+              <p className="text-gray-600 text-2xl">Jméno:</p>
+              <p className="text-lg font-medium">{session.user.username || "Neznámé jméno"}</p>
             </div>
-          )}
+
+            <div className="w-full text-left">
+              <p className="text-gray-600 text-2xl">Email:</p>
+              <p className="text-lg font-medium">{session.user.email}</p>
+            </div>
+
+            {session.user.role && (
+              <div className="w-full text-left">
+                <p className="text-gray-600 text-2xl">Role:</p>
+                <p className="text-lg font-medium">{session.user.role}</p>
+              </div>
+            )}
+
+            <div className="w-full flex flex-col items-center p-5">
+              <div className="w-full flex justify-center mb-4">
+                <div className="w-32 h-[2px] bg-gray-600" />
+              </div>
+              <button 
+                disabled={status === "loading"}
+                onClick={() => signOut()}
+                className="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-500 hover:cursor-pointer focus-visible:outline-red-600"
+              >
+                Odhlásit se
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center items-center h-full bg-white shadow-md rounded-lg w-3/4 m-10 mt-25">
+          {/* <div className="flex flex-col gap-4">
+              <div>
+                <p className="text-gray-600 text-sm">Jméno:</p>
+                <p className="text-lg font-medium">{session.user.username || "Neznámé jméno"}</p>
+              </div>
+              <div>
+                <p className="text-gray-600 text-sm">Email:</p>
+                <p className="text-lg font-medium">{session.user.email}</p>
+              </div>
+              {session.user.role && (
+                <div>
+                  <p className="text-gray-600 text-sm">Role:</p>
+                  <p className="text-lg font-medium">{session.user.role}</p>
+                </div>
+              )}
+            </div> */}
+            <div>
+              <p className="text-gray-600 text-2xl">Balíčky:</p>
+              <p className="text-lg font-medium">Zde budou vaše balíčky a karty.</p>
+              <p className="text-lg font-medium">Zatím žádné nemáte.</p>
+              <p className="text-lg font-medium">Pokud chcete nějaké mít, otevřete si je.</p>
+            </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
